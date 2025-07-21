1a3b2979e6d78687957fa3efcdd1efd8e1004767
Ifeoma <mizfee09@gmail.com>
2025-07-21 12:12:04 +0100

    Made

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,10 +1,14 @@
-const UserProfile = (props) => {
-    console.log(props)
+import React, { useContext } from 'react';
+import { UserContext } from './UserContext';
+
+
+const UserProfile = () => {
+    const { user } = useContext(UserContext);
     return (
-        <div>
-        <h2>{props.name}</h2>
-        <p>Age: {props.age}</p>
-        <p>Bio: {props.bio}</p>
+        <div style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
+            <h2 style={{ color: 'blue' }}>{user.name}</h2>
+            <p>Age: <span style={{ fontWeight: 'bold' }}>{user.age}</span></p>
+            <p>Bio: {user.bio}</p>
         </div>
     );
 };
