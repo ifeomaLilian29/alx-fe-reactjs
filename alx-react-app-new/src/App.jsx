0431fc8639338eab975487054d71dc529395405b
Ifeoma <mizfee09@gmail.com>
2025-07-21 11:54:08 +0100

    added changes

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css'
-import WelcomeMessage from '../src/components/WelcomeMessage'
-import Header from '../src/components/Header'
-import MainContent from '../src/components/MainContent'
-import Footer from  './components/Footer' 
+import WelcomeMessage from './components/WelcomeMessage'
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer' 
 import UserProfile  from './components/UserProfile' 
+import Counter from './components/Counter'
 
 function App() {
   return (
@@ -11,8 +12,10 @@ function App() {
       <WelcomeMessage />
       <Header />
       <MainContent />
-      <Footer />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      <Counter />
+      <Footer />
+      
     </>
   )
 }
