276edf06293d9cafa6bff80a46d920bf1a2d9d9b
Ifeoma <mizfee09@gmail.com>
2025-08-03 19:25:29 +0100

    Added advanced search filters and UI

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://api.github.com/search/users";
+const BASE_URL = "https://api.github.com/search/users?q";
 const API_KEY = import.meta.env.VITE_APP_GITHUB_API_KEY;
 
 export const fetchUserData = async ({ username, location, minRepos, page = 1 }) => {
